f77d09aa75d38e36875f518a94d58fc2102ccfb9
LaurentCAILLAUD <laurent.caillaud@icloud.com>
2023-02-20 17:55:11 +0100

    Another commit after creation of all tables for the front-end

--- databeseStructure.sql
+++ databeseStructure.sql
@@ -21,7 +21,7 @@ CREATE TABLE
         password VARCHAR(60) NOT NULL
     );
 
-/*Concernant ma classe Movie (table movies) toutes les propriétés de type String, sauf description, seront typées en VARCHAR avec 255 caractères. La propriété description stockera la description du film. De part sa fonction cette propriété pourra contenir un long text (plus de 255 caractères). Pour cette raison dans ma table movies, je choisi de typer le champ description en TEXT. Le champ castList sera typée en JSON car ma base de donnée ne possède pas de type ARRAY. */
+/*Concernant ma classe Movie (classe movies) toutes les propriétés de type String, sauf description, seront typées en VARCHAR avec 255 caractères. La propriété description stockera la description du film. De part sa fonction cette propriété pourra contenir un long text (plus de 255 caractères). Pour cette raison dans ma table movies, je choisi de typer le champ description en TEXT. Le champ castList sera typée en JSON car ma base de données ne possède pas de type ARRAY. */
 
 CREATE TABLE
     IF NOT EXISTS movies (
@@ -35,7 +35,7 @@ CREATE TABLE
         director_name VARCHAR(255) NOT NULL
     );
 
-/*Je peux maintenant crée ma table bookings correspondant à ma table Booking. Les propriétés fullPricePlace, reducedPricePlace et under14Place seront des champs de type Int(3). Je choisi de mettre 3 en longeur même si il est impossible qu'un client est réservé plus de 999 places. Pour ce qui est de la propriété totalMount celle-ci sera typée en DECIMAL dans la table avec en longueur avant la virgule 4 chiffres et après la virgule 2 chiffres. Enfin la propriété isPaid sera typée,elle, en BOOLEAN. Et pour finir la création de cette table, je crée les champs reserved_by et reserved qui seront respectivement  la clé étrangére faisant référence à la table customers et la clé étrangère faisant référence à la table movies.*/
+/*Je peux maintenant crée ma table bookings correspondant à ma classe Booking. Les propriétés fullPricePlace, reducedPricePlace et under14Place seront des champs de type Int(3). Je choisi de mettre 3 en longeur même si il est impossible qu'un client est réservé plus de 999 places. Pour ce qui est de la propriété totalMount celle-ci sera typée en DECIMAL dans la table avec en longueur avant la virgule 4 chiffres et après la virgule 2 chiffres. Enfin la propriété isPaid sera typée,elle, en BOOLEAN. Et pour finir la création de cette table, je crée les champs reserved_by et reserved qui seront respectivement  la clé étrangére faisant référence à la table customers et la clé étrangère faisant référence à la table movies.*/
 
 CREATE TABLE
     IF NOT EXISTS bookings (
@@ -50,3 +50,63 @@ CREATE TABLE
         FOREIGN KEY (reserved_by) REFERENCES custumers(id),
         FOREIGN KEY (reserved) REFERENCES movies(id)
     );
+
+/* Afin de continuer dans la relation One To Many, j'observe que la classe MovieRoom posséde cette relation avec la classe Movie. Je décide donc de créer une table movies_room correspondant à la classe MovieRoom. Rien de compliqué dans le typage des champs de ma table et il faut que je crée un champ projected faisant référence à la table movies. */
+
+CREATE TABLE
+    IF NOT EXISTS movies_room (
+        id CHAR(36) NOT NULL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        number_of_place INT(4) NOT NULL,
+        projected CHAR(36) NOT NULL,
+        FOREIGN KEY (projected) REFERENCES movies(id)
+    );
+
+/*Je passe maintenant à la classe Cinema. Je crée donc une table cinemas. Cette table posséde une relation de type Many To Many. Afin de gérer cette relation je vais créer une table associative que je nomme on_display et qui géréra l'association en les tables movies et cinemas avec une clé étranére par tables. Le typage des champ de ma table cinemas ne pose pas de souci et comporte que des types vus déjà précédemment. */
+
+CREATE TABLE
+    IF NOT EXISTS cinemas (
+        id CHAR(36) NOT NULL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        address VARCHAR(255) NOT NULL,
+        zip_code INT(5) NOT NULL,
+        town VARCHAR(255) NOT NULL,
+        phone_number VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        numberOfMoviesRoom INT(3) NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS on_display (
+        movie_id CHAR(36) NOT NULL,
+        cinema_id CHAR(36) NOT NULL,
+        PRIMARY KEY (movie_id, cinema_id),
+        FOREIGN KEY (movie_id) REFERENCES movies (id),
+        FOREIGN KEY (cinema_id) REFERENCES cinemas(id)
+    );
+
+/* Je m'attaque maintenant à la classe SessionSchedule en créant une table sessions_schedule. Cette dernière possède également une relation de type Many To Many avec la table movies. Je crée donc une table associative que je nomme projected_film. */
+
+CREATE TABLE
+    IF NOT EXISTS sessions_schedule (
+        id CHAR(36) NOT NULL PRIMARY KEY,
+        session DATETIME NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS projected_film (
+        movie_id CHAR(36) NOT NULL,
+        session_schedule_id CHAR(36) NOT NULL,
+        PRIMARY KEY (movie_id, session_schedule_id),
+        FOREIGN KEY (movie_id) REFERENCES movies (id),
+        FOREIGN KEY (session_schedule_id) REFERENCES sessions_schedule(id)
+    );
+
+/* Pour finir la partie front-end, il me reste à créer la table prices qui correspond à la classe Price. Cette classe n'a aucune relation avec une autre table.*/
+
+CREATE TABLE
+    IF NOT EXISTS prices (
+        id CHAR(36) NOT NULL PRIMARY KEY,
+        price_category VARCHAR(255) NOT NULL,
+        price DECIMAL(4, 2) NOT NULL
+    )
